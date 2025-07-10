dbdb2235e36b38f888b8352ed725b8365deeb70a
FrankFuAM <98180142+FrankFuAM@users.noreply.github.com>
2025-07-10 11:48:54 +0800

    remove app code

--- App/Android/app/src/main/java/com/hyun/robot/ui/DeviceState.kt
+++
@@ -1,9 +0,0 @@
-package com.hyun.robot.ui
-
-import android.bluetooth.BluetoothDevice
-
-class DeviceState {
-   var isToggleOn :Boolean = false
-    var bDevice: BluetoothDevice? = null
-    var showLoading :Boolean = true
-}
